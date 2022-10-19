2328dbbc02e20109d0a5472d9a91a6389ca48cdd
whoknowsi <echosmania@gmail.com>
2022-10-19 03:55:20 -0300

    uibuild

--- models/person.js
+++ models/person.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose")
 
 const url = process.env.MONGODB_URI
 
+console.log(process.env.MONGODB_URI)
+
 mongoose.connect(url)
     .then(result => {
         console.log("connected to MongoDB")
