0652aea10f3ec5b08482a250ebb726367410b4b5
whoknowsi <echosmania@gmail.com>
2022-10-19 09:41:51 -0300

    part3

--- models/person.js
+++ models/person.js
@@ -1,34 +1,34 @@
-const mongoose = require("mongoose")
-const uniqueValidator = require("mongoose-unique-validator")
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URI
 
 mongoose
 	.connect(url)
-	.then( () => {
-		console.log("connected to MongoDB")
+	.then(() => {
+		console.log('connected to MongoDB')
 	})
 	.catch((error) => {
-		console.log("error connecting to MongoDB: ", error.message)
+		console.log('error connecting to MongoDB: ', error.message)
 	})
 
 const personSchema = new mongoose.Schema({
 	name: {
 		type: String,
-		minlength: [3, "must be at least 3 characters"],
+		minlength: [3, 'must be at least 3 characters'],
 		required: true,
 		unique: true,
 	},
 	number: {
 		type: String,
-		minlength: [8, "must be at least 8 digits"],
+		minlength: [8, 'must be at least 8 digits'],
 		required: true,
 	},
 })
 
 personSchema.plugin(uniqueValidator)
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
 	transform: (document, returnedObject) => {
 		returnedObject.id = returnedObject._id.toString()
 		delete returnedObject._id
@@ -36,4 +36,4 @@ personSchema.set("toJSON", {
 	},
 })
 
-module.exports = mongoose.model("Person", personSchema)
+module.exports = mongoose.model('Person', personSchema)
